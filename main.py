7b9dc814444c51ec1a4a9dfee5f45c0751f0f5ce
Vijay Kumar M R <83636006+Vijay093@users.noreply.github.com>
2023-08-25 15:46:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ Bootstrap(app)
 # CONNECT TO DB
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI",'sqlite:///blogs.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DB_URI']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 db.init_app(app)
 
