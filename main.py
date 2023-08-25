398c1d2392ec3fdd2a7207a8d6566b79ded368e5
Vijay Kumar M R <83636006+Vijay093@users.noreply.github.com>
2023-08-25 16:17:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     posts = relationship("BlogPost", back_populates="author")
     comments = relationship("Comment", back_populates="comment_author")
 
